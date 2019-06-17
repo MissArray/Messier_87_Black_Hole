362ea970a03af78beb44babfddd1905484c0d9a8
Miss Array <miss.arrary42@gmail.com>
2019-06-17 17:30:37 +0100

    Added basic info / large screens

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleInfo() {
+    let infoP = document.getElementById('info');
+    if (infoP.style.display === 'none') {
+        infoP.style.display = 'block';
+    }
+    else {
+        infoP.style.display = 'none';
+    }
+}
